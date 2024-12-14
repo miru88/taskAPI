3040cb1aeb08d6cca36d6c5870ba5204e04bba5e
Guillermo Gutierrez <miru.chohi@gmail.com>
2024-12-13 23:56:59 -0500

    added @Public() decorator to login endpoint

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -2,12 +2,15 @@ import { Body, Controller, Post, HttpCode, HttpStatus, UseGuards, Request  } fro
 import { AuthService } from 'src/services/auth.service';
 import { SignInDto } from '../dtos/user.dto';
 import { LocalAuthGuard } from '../guards/local-auth.guard';
+import { JwtAuthGuard } from '../guards/jwt-auth.guard';
+import { Public } from '../decorators/decorators.index';
 
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService) {}
 
     @HttpCode(HttpStatus.OK)
+    @Public()
     @UseGuards(LocalAuthGuard)
     @Post('login')
     signIn(@Body() signInDto: SignInDto) {
@@ -16,9 +19,10 @@ export class AuthController {
 
 
 
-    @UseGuards(LocalAuthGuard)
+    @UseGuards(JwtAuthGuard)
     @Post('logout')
     async logout(@Request() req) {
+        return 'logged out'
         return req.logout();
       }
 
